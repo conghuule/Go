92a6ec9f9363a000591523528052a584658891c5
minhtringuyennn <contact@minhtringuyennn.com>
2022-09-30 13:08:27 +0700

    Finalize Week 04 Assignment

--- Week 04/CustomListDemo/app/src/main/java/com/example/customlistdemo/MainActivity.java
+++ Week 04/CustomListDemo/app/src/main/java/com/example/customlistdemo/MainActivity.java
@@ -1,28 +1,17 @@
 package com.example.customlistdemo;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
-import android.app.ListActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
-
-import java.util.List;
 
 public class MainActivity extends Activity {
     TextView selectedItem;
     ListView listView;
 
-    String[] names = {"Nguyễn Văn A", "Lê Thị B", "Trần Văn C", "Phan Văn C", "Đinh Văn D"};
-    String[] phones = {"0989897873", "0967995843", "0907955843", "0967855411", "0988885231"};
+    String[] names = {"Nguyễn Minh Trí", "Lê Công Hửu", "Lê Tấn Kiệt", "Ngô Thanh Lực", "Nguyễn Long Vũ"};
+    String[] phones = {"0911271505", "0817433090", "0817433090", "0793652374", "0356436545"};
     Integer[] avatars = {R.drawable.ic_avatar_1, R.drawable.ic_avatar_2, R.drawable.ic_avatar_3, R.drawable.ic_avatar_4, R.drawable.ic_avatar_5};
-
     People[] peoples = {
             new People(avatars[0], names[0], phones[0]),
             new People(avatars[1], names[1], phones[1]),
@@ -34,20 +23,19 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
-        selectedItem = (TextView) findViewById(R.id.text);
-        listView = (ListView) findViewById(R.id.list);
+        selectedItem = findViewById(R.id.text);
+        listView = findViewById(R.id.list);
 
         CustomAdapter adapter = new CustomAdapter(this, R.layout.custom_row_icon_label, peoples);
 
         listView.setAdapter(adapter);
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                selectedItem.setText("You choose: " + peoples[i].getName());
-            }
+        listView.setOnItemClickListener((adapterView, view, i, l) -> {
+            String choose_msg = "You choose: " + peoples[i].getName();
+            selectedItem.setText(choose_msg);
         });
     }
 }
