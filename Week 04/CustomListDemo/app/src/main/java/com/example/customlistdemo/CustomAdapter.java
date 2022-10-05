d7bb8171938b808fc2b63975ecf8896ac20db81f
20120405 <longvu2907@gmail.com>
2022-10-05 18:56:37 +0700

    Revert "Finalize Week 04 Assignment"
    This reverts commit 92a6ec9f9363a000591523528052a584658891c5.

--- Week 04/CustomListDemo/app/src/main/java/com/example/customlistdemo/CustomAdapter.java
+++ Week 04/CustomListDemo/app/src/main/java/com/example/customlistdemo/CustomAdapter.java
@@ -12,24 +12,23 @@ import android.widget.TextView;
 
 public class CustomAdapter extends ArrayAdapter<People> {
     Context context;
-    int layoutToBeInflated;
     People[] peoples;
 
     public CustomAdapter(Context context, int layoutToBeInflated, People[] peoples) {
         super(context, layoutToBeInflated, peoples);
+        System.out.println("context " + context);
         this.context = context;
-        this.layoutToBeInflated = layoutToBeInflated;
         this.peoples = peoples;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         LayoutInflater inflater = ((Activity) context).getLayoutInflater();
-        View row = inflater.inflate(layoutToBeInflated, null);
+        View row = inflater.inflate(R.layout.custom_row_icon_label, null);
 
-        ImageView avatar = row.findViewById(R.id.avatar);
-        TextView name = row.findViewById(R.id.name);
-        TextView phone = row.findViewById(R.id.phone);
+        ImageView avatar = (ImageView) row.findViewById(R.id.avatar);
+        TextView name = (TextView) row.findViewById(R.id.name);
+        TextView phone = (TextView) row.findViewById(R.id.phone);
 
         People people = peoples[position];
         avatar.setImageResource(people.getAvatar());
