430cf915d9ce0c03d593ca2ba33dc68958c19c72
20120294 <20120294@student.hcmus.edu.vn>
2022-11-21 23:18:28 +0700

    create profileActivity

---
+++ Final Project/Go/app/src/main/java/com/company/go/Activities/profileActivity.java
@@ -0,0 +1,75 @@
+package com.company.go.Activities;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import com.company.go.R;
+
+public class profileActivity extends Activity {
+    private ImageView btnBack;
+    private TextView btnLogout;
+    private LinearLayout btnEdit_Pro;
+    private LinearLayout btnEdit_Pass;
+    private ConstraintLayout mainScreen;
+    private LinearLayout loginScreen;
+    private LinearLayout editProfileScreen;
+    private LinearLayout editPassScreen;
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        btnBack = (ImageView) findViewById(R.id.btnBack);
+        btnLogout =(TextView) findViewById(R.id.btnLogout);
+        btnEdit_Pro=(LinearLayout) findViewById(R.id.btnEditPro);
+        btnEdit_Pass=(LinearLayout)findViewById(R.id.btnEditPass);
+        mainScreen =(ConstraintLayout) findViewById(R.id.activity_main);
+        loginScreen =(LinearLayout) findViewById(R.id.activity_login);
+        editProfileScreen=(LinearLayout)findViewById(R.id.activity_editPro1);
+        editPassScreen=(LinearLayout)findViewById(R.id.activity_editPass);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent_Profile2Main = new Intent(profileActivity.this, mainScreen.getClass());
+                startActivity(myIntent_Profile2Main);
+                finish();
+            }
+        });
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent_Profile2Login = new Intent(profileActivity.this, loginScreen.getClass());
+                startActivity(myIntent_Profile2Login);
+                finish();
+            }
+        });
+
+        btnEdit_Pro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent_Profile2EditPro = new Intent(profileActivity.this, editProfileScreen.getClass());
+                startActivity(myIntent_Profile2EditPro);
+                finish();
+            }
+        });
+
+        btnEdit_Pass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent_Profile2EditPass = new Intent(profileActivity.this, editPassScreen.getClass());
+                startActivity(myIntent_Profile2EditPass);
+                finish();
+            }
+        });
+    }
+
+}
--- Final Project/Go/app/src/main/java/com/company/go/Fragments/Account.java
+++ Final Project/Go/app/src/main/java/com/company/go/Fragments/Account.java
@@ -1,66 +1,87 @@
 package com.company.go.Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.company.go.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link Account#newInstance} factory method to
+ * Use the {@link Account #newInstance} factory method to
  * create an instance of this fragment.
  */
 public class Account extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public Account() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Account.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Account newInstance(String param1, String param2) {
-        Account fragment = new Account();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private ImageView btnBack;
+    private TextView btnLogout;
+    private LinearLayout btnEdit_Pro;
+    private LinearLayout btnEdit_Pass;
+    private ConstraintLayout mainScreen;
+    private LinearLayout loginScreen;
+    private LinearLayout editProfileScreen;
+    private LinearLayout editPassScreen;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_account, container, false);
+    }
+
+    @Override
+    public void onCreate(Bundle savedlnstanceState) {
+
+        super.onCreate(savedlnstanceState);
+
+        btnBack = (ImageView) btnBack.findViewById(R.id.btnBack);
+        btnLogout =(TextView) btnLogout.findViewById(R.id.btnLogout);
+        btnEdit_Pro=(LinearLayout) btnEdit_Pro.findViewById(R.id.btnEditPro);
+        btnEdit_Pass=(LinearLayout)btnEdit_Pass.findViewById(R.id.btnEditPass);
+        mainScreen =(ConstraintLayout) mainScreen.findViewById(R.id.activity_main);
+        loginScreen =(LinearLayout) loginScreen.findViewById(R.id.activity_login);
+        editProfileScreen=(LinearLayout)editProfileScreen.findViewById(R.id.activity_editPro1);
+        editPassScreen=(LinearLayout)editPassScreen.findViewById(R.id.activity_editPass);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent_Profile2Main = new Intent(getActivity(), mainScreen.getClass());
+                startActivity(myIntent_Profile2Main);
+            }
+        });
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent_Profile2Login = new Intent(getActivity(), loginScreen.getClass());
+                startActivity(myIntent_Profile2Login);
+            }
+        });
+
+        btnEdit_Pro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent_Profile2EditPro = new Intent(getActivity(), editProfileScreen.getClass());
+                startActivity(myIntent_Profile2EditPro);
+            }
+        });
+
+        btnEdit_Pass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent_Profile2EditPass = new Intent(getActivity(), editPassScreen.getClass());
+                startActivity(myIntent_Profile2EditPass);
+            }
+        });
     }
 }
